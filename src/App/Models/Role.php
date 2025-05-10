126e4851a300e198d22b355db4e5c988332d754f
callmeaf <alikhoshkar2000@gmail.com>
2025-05-10 11:52:08 +0330

    change role name cast

--- src/App/Models/Role.php
+++ src/App/Models/Role.php
@@ -5,6 +5,7 @@ namespace Callmeaf\Role\App\Models;
 use Callmeaf\Base\App\Models\BaseModel;
 use Callmeaf\Base\App\Models\Contracts\BaseConfigurable;
 use Callmeaf\Base\App\Traits\Model\BaseModelMethods;
+use Callmeaf\Role\App\Enums\RoleName;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\App;
@@ -20,20 +21,38 @@ class Role extends \Spatie\Permission\Models\Role implements BaseConfigurable
 
     protected function casts(): array
     {
+        $enumsCast = self::config()['enums'] ?? [];
+        unset($enumsCast['name']);
         return [
-            ...(self::config()['enums'] ?? []),
+            ...$enumsCast,
         ];
+    }
+
+    public function name(): Attribute
+    {
+        return Attribute::get(
+            fn($value) => RoleName::tryFrom($value) ?: $value,
+        );
     }
 
     public function nameText(): Attribute
     {
         return Attribute::get(function() {
+            $currentLocale = App::currentLocale();
+
+            if(! ($this->name instanceof \BackedEnum)) {
+                if($currentLocale === 'fa') {
+                    return $this->name_fa ?? $this->name;
+                }
+                return $this->name;
+            }
+
             $enumTranslate = \Base::enumCaseTranslator(
                 \Base::getPackageNameFromModel(model: self::class),
                 $this->name,
             );
 
-            if(App::currentLocale() === 'fa') {
+            if($currentLocale === 'fa') {
                 return $this->name_fa ?? $enumTranslate;
             }
 
