4c3af33f652464b755459f146ee53eb4d0c16440
callmeaf <alikhoshkar2000@gmail.com>
2025-05-30 20:16:16 +0330

    add HasDate for permission

--- src/App/Http/Resources/Admin/V1/RoleResource.php
+++ src/App/Http/Resources/Admin/V1/RoleResource.php
@@ -29,8 +29,11 @@ class RoleResource extends JsonResource
             'name_fa' => $this->name_fa,
             'guard_name' => $this->guard_name,
             'created_at' => $this->created_at,
+            'created_at_text' => $this->createdAtText(),
             'updated_at' => $this->updated_at,
+            'updated_at_text' => $this->updatedAtText(),
             'deleted_at' => $this->deleted_at,
+            'deleted_at_text' => $this->deletedAtText(),
             'permissions' => $permissionRepo->toResourceCollection($this->whenLoaded('permissions'))
         ];
     }
--- src/App/Models/Role.php
+++ src/App/Models/Role.php
@@ -5,6 +5,7 @@ namespace Callmeaf\Role\App\Models;
 use Callmeaf\Base\App\Models\BaseModel;
 use Callmeaf\Base\App\Models\Contracts\BaseConfigurable;
 use Callmeaf\Base\App\Traits\Model\BaseModelMethods;
+use Callmeaf\Base\App\Traits\Model\HasDate;
 use Callmeaf\Role\App\Enums\RoleName;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\App;
 
 class Role extends \Spatie\Permission\Models\Role implements BaseConfigurable
 {
-    use BaseModelMethods;
+    use BaseModelMethods,HasDate;
 
     public static function configKey(): string
     {
